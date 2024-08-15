cb6005b0ca034d22bf3706c2f6fe322caee4020f
Dongju Yeon <dongju1497@gmail.com>
2024-08-16 03:24:52 +0900

    feat: Initialize project with base module, TypeORM, and Docker setup
    - Added initial project structure with a core module.
    - Integrated TypeORM for database management.
    - Configured Docker with PostgreSQL container for development environment.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5555,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+    CommonModule,
+    AuthModule,
+    UserModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
