9467e8e4986591b6d36ef2cafb30875d116a784a
Dongju Yeon <dongju1497@gmail.com>
2024-09-17 16:44:55 +0900

    fix: error handling and environment configuration for development/production

--- src/common/const/env-keys.const.ts
+++ src/common/const/env-keys.const.ts
@@ -1,6 +1,6 @@
 // 서버 프로토콜 -> http / https
 export const ENV_PROTOCOL_KEY = 'PROTOCOL';
-// 서버 호스트 -> localhost:3000
+// 서버 호스트 -> localhost:8000
 export const ENV_HOST_KEY = 'HOST';
 // JWT 토큰 시크릿 -> codefactory
 export const ENV_JWT_SECRET_KEY = 'JWT_SECRET';
